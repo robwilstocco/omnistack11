ea129bec200b099a15c852888f2d5292362f547e
Robert Wilson <robwilstocco@gmail.com>
2020-05-29 18:29:43 -0300

    algumas alteracoes no frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Logon from './pages/Logon/index';
+import './global.css';
+
+// JSX (Javascript XML) -- html integrado em um arquivo .js
+//componente é uma função que retorna html.
 
 function App() {
+  /*
+    let [counter,setCounter] = useState(0); // retorna um Array [valor, funcaoDeAtualizacao]
+    function increment(){
+      setCounter(counter + 1);
+      console.log(counter);
+    }
+  */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Logon/>
   );
 }
 
