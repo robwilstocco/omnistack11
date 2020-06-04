913ab26aae32bc1238804cb403796f7a0654026d
Robert Wilson <robwilstocco@gmail.com>
2020-06-04 00:12:30 -0300

    mobile finalizado

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -7,7 +7,7 @@ module.exports = {
         const [count] = await connection ('incidents').count();
 
         const incidents = await connection ('incidents')
-            .join('ongs','ong_id','=','incidents.ong_id')
+            .join('ongs','ongs.id','=','incidents.ong_id')
             .limit(5)
             .offset((page - 1) * 5)
             .select([
@@ -48,7 +48,7 @@ module.exports = {
             .select ('ong_id')
             .first();
 
-        if (incident.ong_id != ong_id){
+        if (incident.ong_id !== ong_id){
             return response.status(401).json({error: 'Operation not permited.'});
         }
 
